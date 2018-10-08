6f896b3027047850373a09df5ae53f93277f048b
郜家强 <gaojiaqiang@chinasofti.com>
2018-10-08 21:41:49 +0800

    Initial commit
    页面提交

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,95 @@
+const app = getApp()
+
+Page({
+  data: {
+    progress_txt: '20%',
+    count: 0, // 设置 计数器 初始为0
+    countTimer: null, // 设置 定时器 初始为nul
+    isChecked: false
+  },
+  openBluetooth() {
+    console.log("打开蓝牙按钮");
+  },
+
+  switchBlueTooth() {
+    wx.redirectTo({
+      url: 'blue'
+    })
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数 
+  },
+  onReady: function () {
+
+  },
+  onShow: function () {
+    // 页面显示 
+  },
+  onHide: function () {
+    // 页面隐藏 
+  },
+  onUnload: function () {
+    // 页面关闭 
+  },
+
+  drawProgressbg: function () {
+    // 使用 wx.createContext 获取绘图上下文 context
+    var ctx = wx.createCanvasContext('canvasProgressbg')
+    ctx.setLineWidth(4);// 设置圆环的宽度
+    ctx.setStrokeStyle('#20183b'); // 设置圆环的颜色
+    ctx.setLineCap('round') // 设置圆环端点的形状
+    ctx.beginPath();//开始一个新的路径
+    ctx.arc(55, 55, 50, 0, 2 * Math.PI, false);
+    //设置一个原点(100,100)，半径为90的圆的路径到当前路径
+    ctx.stroke();//对当前路径进行描边
+    ctx.draw();
+  },
+  drawCircle: function (step) {
+    var context = wx.createCanvasContext('canvasProgress');
+    // 设置渐变
+    var gradient = context.createLinearGradient(100, 50, 50, 100);
+    gradient.addColorStop("0", "#2661DD");
+    gradient.addColorStop("0.5", "#40ED94");
+    gradient.addColorStop("1.0", "#5956CC");
+
+    context.setLineWidth(10);
+    context.setStrokeStyle(gradient);
+    context.setLineCap('round')
+    context.beginPath();
+    // 参数step 为绘制的圆环周长，从0到2为一周 。 -Math.PI / 2 将起始角设在12点钟位置 ，结束角 通过改变 step 的值确定
+    context.arc(55, 55, 50, -Math.PI / 2, step * Math.PI - Math.PI / 2, false);
+    context.stroke();
+    context.draw()
+  },
+  onReady: function () {
+    this.drawProgressbg();
+
+    this.drawCircle(2);
+  },
+
+
+})
+
+
+
+const deviceNameHC ="HC-05";
+function inArray(arr, key, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][key] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// ArrayBuffer转16进度字符串示例
+function ab2hex(buffer) {
+  var hexArr = Array.prototype.map.call(
+    new Uint8Array(buffer),
+    function (bit) {
+      return ('00' + bit.toString(16)).slice(-2)
+    }
+  )
+  return hexArr.join('');
+}
+
